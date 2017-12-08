4ec9d4312871c0c4f640d3f09a4875a47f4c3a2e
earnor <ADMIN@129.132.211.63>
2017-12-08 09:49:29 +0100

    Some thoughts and structuring

--- src/main/java/ch/ethz/matsim/courses/abmt17_template/ChangePopulation.java
+++ src/main/java/ch/ethz/matsim/courses/abmt17_template/ChangePopulation.java
@@ -10,22 +10,31 @@ import org.matsim.api.core.v01.population.Population;
 import org.matsim.core.population.io.PopulationWriter;
 
 public class ChangePopulation {
-public void ChangePop ( Scenario scenario ) {
-	Population population = scenario.getPopulation();
-	for(Person person : population.getPersons().values()) {
-		Plan plan = person.getSelectedPlan();
-		for( PlanElement pe : plan.getPlanElements()) {
-			if( pe instanceof Activity) {
-				Activity activity = (Activity) pe;
-			} else {
-				Leg leg = (Leg) pe;
+	public void ChangePop ( Scenario scenario, double carOwnership ) {
+		Population population = scenario.getPopulation();
+		// carOwnership = this.carOwnership;
+		for(Person person : population.getPersons().values()) {
+			// Here we decide which part of the population we want to change
+			// i.e. as a function of carOwnership
+			Plan plan = person.getSelectedPlan();
+			for( PlanElement pe : plan.getPlanElements()) {
+				if( pe instanceof Activity) {
+					Activity activity = (Activity) pe;
+				} else {
+					Leg leg = (Leg) pe;
+					// Assume those owning car will remain unchanged
+					// Assign rand() to each person and set Mode according to the rand value
+					// rand values are 0-0.63 AV, 0.63-0.98 PT, 0.98-1.00 Bike
+					// Should all these values only be applied by Trip purpose?
+					// Our survey was only for work-/educ.-related trips.
+					leg.setMode(arg0);
+				}
+				// Do we compile toe activities and legs again into pe ?
 				
 			}
 		}
-	}
-	PopulationWriter pw = new PopulationWriter (population);
-	pw.write("/home/floriafa/output/pop.txt");
-	
-}	
+		PopulationWriter pw = new PopulationWriter (population);
+		pw.write("/home/floriafa/output/pop.txt");
+	}	
 }	
 
